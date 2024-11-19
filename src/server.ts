c474f5664506f2a8de7b2477372ea324a6cbc48c
mdadnanshuvo <ahmedadnanshuvo@gmail.com>
2024-11-19 19:01:52 +0600

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
-// src/server.ts
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import hotelRoutes from './routes/hotelRoute';
 import imageRoutes from './routes/imageRoute';
+import path from 'path';
 
 export const app = express();
 const PORT = 3000;
 
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'http://localhost:3001', // Allow requests from the frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+}));
+
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'uploads' folder
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
 // Routes
 app.use('/', hotelRoutes);
